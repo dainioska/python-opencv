6a507e743ab3ced2bf7c6cff4e79aca3c8674fd9
dainioshka <dainioshka@gmail.com>
2021-03-03 08:48:44 +0200

    add file

--- p_know_03.py
+++ p_know_03.py
@@ -1,8 +1,12 @@
-#paint goemetric to image
+#paint goemetric to image and  numpy field
 import cv2
+import numpy as np
 
-img = cv2.imread('lena.jpg', 1)
+#img = cv2.imread('lena.jpg', 1)
+img = np.zeros([512, 512, 3], np.uint8)
+
 img =cv2.line(img, (0,0), (255, 255), (255,0,0), 5)
+img = cv2.circle(img, (475, 65),65, (0, 255,0), -1)
 cv2.imshow('image', img)
 
 k = cv2.waitKey(0)
--- p_know_04.py
+++ p_know_04.py
@@ -1,19 +1,15 @@
-#usb_video stream show and write to file
+#usb_video stream parameters
 import cv2
 
 cap = cv2.VideoCapture(0)   # usb port number
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
-#cap = cv2.VideoCapture('output.avi')
+cap.set(3, 1208)
+cap.set(4, 720)
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-#print(cap.isOpened())
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret ==True:
-       print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-       print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-       out.write(frame)
 
        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
        cv2.imshow('frame', gray)
@@ -24,7 +20,6 @@ while(cap.isOpened()):
         break
 
 cap.realease()
-out.realease()
 cv2.destroyAllWindows()
 
 
