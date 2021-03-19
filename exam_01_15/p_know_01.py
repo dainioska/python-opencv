3d85f0a540ed6a0e70ffd17805b35e8bdb8658c4
dainioshka <dainioshka@gmail.com>
2021-03-19 06:46:15 +0200

    dir changes

---
+++ exam_01_15/p_know_01.py
@@ -0,0 +1,12 @@
+#read and show from image
+import cv2
+
+img = cv2.imread('samples/lena.jpg', -1)
+cv2.imshow('Image', img)
+
+k = cv2.waitKey(0)
+if k == 27:
+    cv2.destroyAllWindows()
+elif k == ord('s'):
+    cv2.imwrite('lena_copy.png', img)
+    cv2.destroyAllWindows()
---
+++ exam_other/road_cv2.py
@@ -0,0 +1,31 @@
+import cv2 as cv 
+import numpy as np 
+import lanes
+
+video = cv.VideoCapture("road_01.mp4")
+
+if not video.isOpened():
+    print('error while opennig the video')
+
+cv.waitKey(1)
+
+while video.isOpened():
+    _, frame = video.read()
+    cv.resizeWindow("Video", 1300, 800)
+    copy_img = np.copy(frame)
+
+    frame = lanes.canny(frame)
+    frame =lanes.mask(frame)
+    lines = cv.HoughLinesP(frame, 2, np.pi/180, 100, np.array([()]), minLineLength=20, maxLineGap=5)
+    average_lines = lanes.average_slope_intercept(frame, lines)
+    line_image =lanes.display_lines(copy_img, average_lines)
+    combo = cv.addWeighted(copy_img, 0.8, line_image, 0.5, 1)
+
+    cv.imshow("Video", combo)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        video.release()
+        cv.destroyAllWindows()
+
+video.release()
+cv.destroyAllWindows()
